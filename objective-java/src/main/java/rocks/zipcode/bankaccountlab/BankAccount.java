5b9022b592adcae4fb48e99ec4f875a05bdbd890
joseph <josem.c7@icloud.com>
2026-05-18 14:41:19 -0400

    final commit before laptop transfer

--- objective-java/src/main/java/rocks/zipcode/bankaccountlab/BankAccount.java
+++ objective-java/src/main/java/rocks/zipcode/bankaccountlab/BankAccount.java
@@ -4,4 +4,32 @@ package rocks.zipcode.bankaccountlab;
  * Created by leon on 1/10/18.
  */
 public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount balance : $" + balance;
+    }
+
 }
+
--- objective-java/src/test/java/rocks/zipcode/bankaccountlab/BankAccountTest.java
+++ objective-java/src/test/java/rocks/zipcode/bankaccountlab/BankAccountTest.java
@@ -1,7 +1,12 @@
 package rocks.zipcode.bankaccountlab;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class BankAccountTest {
+
+   
 }
